d1f584b5db6af0ae0982ed98aea0d1032ff38c97
Your Name <tomato@FR-PAR3-W10001.local>
2017-12-18 16:36:47 +0100

    version initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,17 +78,8 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p>Page Welcome</p>
+    
             </div>
         </div>
     </body>
